0d63a275017bca3745c483a3575d124183dbf845
aarush67 <aarushprakash3@gmail.com>
2024-10-19 10:55:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', async () => {
     const videoUrl = document.getElementById('videoUrl').value;
     const resultParagraph = document.getElementById('result');
-    const warningParagraph = document.getElementById('warning');
 
     try {
         const response = await fetch(`https://9c628245-3814-4ecd-a755-6d4de0467c5c-00-3tzwugoleljgd.spock.replit.dev/download?url=${encodeURIComponent(videoUrl)}`);
@@ -10,7 +9,6 @@ document.getElementById('downloadBtn').addEventListener('click', async () => {
         if (response.ok && data.success) {
             const downloadLink = data.link;
             resultParagraph.innerHTML = `Streaming Link: <a href="${downloadLink}" target="_blank">Click here to stream</a>`;
-            warningParagraph.classList.remove('hidden'); // Show the warning
             window.location.href = downloadLink; // Redirect to streaming URL
         } else {
             resultParagraph.textContent = `Error: ${data.error}`;
