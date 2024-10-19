264c26a6ba6b27433b6eed02fab4ab11beb4b2a8
aarush67 <aarushprakash3@gmail.com>
2024-10-18 20:09:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadButton').addEventListener('click', async () => {
     const url = document.getElementById('videoUrl').value;
-    const response = await fetch(`https://9c628245-3814-4ecd-a755-6d4de0467c5c-00-3tzwugoleljgd.spock.replit.dev/download?url=${encodeURIComponent(url)}`);
+    const response = await fetch(`https://9c628245-3814-4ecd-a755-6d4de0467c5c-00-3tzwugoleljgd.spock.replit.dev//download?url=${encodeURIComponent(url)}`);
     const data = await response.json();
 
     const resultDiv = document.getElementById('result');
